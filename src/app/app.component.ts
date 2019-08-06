de522613be944380e4f4434ca0a5bdfb049f64ba
isaul medrano <medranoisaul5@gmail.com>
2019-08-06 20:38:48 +0200

    Creacion de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'isaul';
+  name = 'Isaul';
+  dni = '9271886';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BodyComponent } from './body/body.component';
+import {MatCardModule} from '@angular/material/card';
+import { FooterComponent } from './footer/footer.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, CommonModule, FormsModule, BrowserAnimationsModule, MatToolbarModule, MatCardModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
